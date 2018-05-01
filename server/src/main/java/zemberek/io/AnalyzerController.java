a78aac8a3bcc3eeac621c96aa71febbb68f9bd22
mdakin <mdakin@gmail.com>
2018-05-01 13:16:00 +0200

    Switch to Zemberek 0.12 Add sentence analysis.

--- server/src/main/java/zemberek/io/AnalyzerController.java
+++ server/src/main/java/zemberek/io/AnalyzerController.java
@@ -6,8 +6,10 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import zemberek.morphology.TurkishMorphology;
+import zemberek.morphology.analysis.SentenceAnalysis;
+import zemberek.morphology.analysis.SingleAnalysis;
 import zemberek.morphology.analysis.WordAnalysis;
-import zemberek.morphology.analysis.tr.TurkishMorphology;
 
 @Controller
 @EnableAutoConfiguration
@@ -23,13 +25,20 @@ public class AnalyzerController {
 
     @GetMapping("/analyze")
     @ResponseBody
-    String home(@RequestParam(name="word", required=false, defaultValue="") String word) {
-        List<WordAnalysis> kedicikler = morphology.analyze(word);
-        StringBuilder sb = new StringBuilder(word);
-        for (WordAnalysis analysis : kedicikler ) {
-            sb.append("<div>");
-            sb.append(analysis.formatLong());
-            sb.append("</div>");
+    String home(@RequestParam(name="word", required=false, defaultValue="") String sentence) {
+
+        List<WordAnalysis> analysisList = morphology.analyzeSentence(sentence);
+        StringBuilder sb = new StringBuilder("Input: "  + sentence);
+        for(WordAnalysis wa : analysisList) {
+            sb.append("<div>" + wa.getInput() + "</div>");
+            for (SingleAnalysis sa : wa) {
+                sb.append("<div>" +  sa.formatLong() + "</div>");
+            }
+        }
+        sb.append("Disambiguation result:");
+        SentenceAnalysis disambiguated = morphology.disambiguate(sentence, analysisList);
+        for (SingleAnalysis sa : disambiguated.bestAnalysis()) {
+            sb.append("<div>" +  sa.formatLong() + "</div>");
         }
         return sb.toString();
     }
